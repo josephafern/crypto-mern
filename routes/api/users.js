e31bc0da44c5593e9e27cc06a49d5865049f0d1c
josephafern <joey.a.fern@gmail.com>
2021-12-02 11:42:39 -0800

    Comments

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+//Test route for api/users/test
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
 module.exports = router;
