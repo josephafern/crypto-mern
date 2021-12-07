d8a18d3f5756c46169ab50e249d55336c1548869
josephafern <60451011+josephafern@users.noreply.github.com>
2021-12-07 14:17:58 -0800

    Started work on currency route and model

---
+++ models/Currency.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CurrencySchema = new Schema({
+    name: {
+      type: String,
+      required: true
+    },
+    prices: {
+      type: Array,
+      required: true
+    },
+    allTimeHigh: {
+      type: Number,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    }
+});
